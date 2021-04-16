6660bc60765762c732a11788189f3e1c47f8039f
Jhonatan Sena <jcostasenna@hotmail.com>
2021-04-16 16:55:04 -0300

    finalizando a parte dos configs

--- src/app/shared/state/config/config.reducer.ts
+++ src/app/shared/state/config/config.reducer.ts
@@ -1,4 +1,4 @@
-import { createReducer, on } from '@ngrx/store';
+import { createReducer, on, Action } from '@ngrx/store';
 
 import { Units } from '../../models/units.enum';
 import * as fromConfigActions from './config.actions';
@@ -11,10 +11,14 @@ export const configInitialState: ConfigState = {
   unit: Units.Metric,
 }
 
-export const configReducer = createReducer(
+const reducer = createReducer(
   configInitialState,
   on(fromConfigActions.updateUnit, (state, { unit }) => ({
     ...state,
     unit,
   })),
 );
+
+export function configReducer(state: ConfigState | undefined, action: Action): ConfigState {
+  return reducer(state, action);
+}
